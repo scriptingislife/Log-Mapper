9a112ed28b8594f7e52b3799c387cd15961c2d1c
Nathaniel Beckstead <becksteadn@gmail.com>
2017-12-20 16:23:38 -0500

    Remove timestamp for distinct markers

--- draw_map.py
+++ draw_map.py
@@ -6,26 +6,25 @@ def draw():
     folium_map = folium.Map(location=[24.635246, 2.616971], zoom_start=3, tiles='CartoDB dark_matter')
 
     #Get markers and make_marker for each
-    stats = db.execute("SELECT DISTINCT IP, STAMP, SUCCESS, COUNTRY, CONTINENT, LATITUDE, LONGITUDE from MARKERS")
+    stats = db.execute("SELECT DISTINCT IP, SUCCESS, COUNTRY, CONTINENT, LATITUDE, LONGITUDE from MARKERS")
     for stat in stats:
         ip = stat[0]
-        stamp = stat[1]
-        success = stat[2]
-        country = stat[3]
-        continent = stat[4]
-        latitude = stat[5]
-        longitude = stat[6]
+        success = stat[1]
+        country = stat[2]
+        continent = stat[3]
+        latitude = stat[4]
+        longitude = stat[5]
         if success == 0:
             success = False
         elif success == 1:
             success = True
         
-        make_marker(folium_map, ip, stamp, success, country, continent, latitude, longitude)
+        make_marker(folium_map, ip, success, country, continent, latitude, longitude)
 
     folium_map.save("folium_map.html")
 
 
-def make_marker(map, ip, stamp, success, country, continent, latitude, longitude):
+def make_marker(map, ip, success, country, continent, latitude, longitude):
     print("Making marker for: "+ str(ip))
     popup_text = """{}<br>
                     Timestamp: {}<br>
