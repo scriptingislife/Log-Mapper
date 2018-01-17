87a33c323eba6f75a10e47346afb1d4a729aae44
Nathaniel Beckstead <becksteadn@gmail.com>
2018-01-17 03:07:12 +0000

    Update env variables

--- update.sh
+++ update.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 
-
-#LM_INSTALL_LOC="/home/ec2-user/environment/Log-Mapper"
-
 . "/etc/os-release"
-. "log-mapper.conf"
+. "logmapper.conf"
 
 if [[ "$ID_LIKE" = "debian" ]]; then
 	echo "Copying /var/log/auth.log"
-	sudo cp /var/log/auth.log "$INSTALL_LOC/auth.log"
+	sudo cp /var/log/auth.log "$LM_INSTALL_LOC/auth.log"
 elif [[ "$ID_LIKE" = "rhel fedora" ]]; then
 	echo "Copying /var/log/secure"
-	sudo cp /var/log/secure "$INSTALL_LOC/auth.log"
+	sudo cp /var/log/secure "$LM_INSTALL_LOC/auth.log"
 fi
 
 chmod 444 auth.log
 
 cd $LM_INSTALL_LOC
-sudo -u $LM_MAPPER_USER python "$INSTALL_LOC/stats_map.py"
-sudo -u $LM_MAPPER_USER python "$INSTALL_LOC/draw_map.py"
-sudo -u $LM_MAPPER_USER python3 "$INSTALL_LOC/get_stats.py"
+sudo -u $LM_MAPPER_USER python "$LM_INSTALL_LOC/stats_map.py"
+sudo -u $LM_MAPPER_USER python "$LM_INSTALL_LOC/draw_map.py"
+sudo -u $LM_MAPPER_USER python3 "$LM_INSTALL_LOC/get_stats.py"
